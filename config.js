3d39dec03fb73510283a95a8c53d5fcccfb6cb86
Filipe-Bina <filipebina4@gmail.com>
2026-06-10 23:02:05 -0300

    fix: migra infraestrutura para novo cluster ativo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GTD_CONFIG = {
-    SUPABASE_URL: "https://pdjgzegvjoxuovfukfmx.supabase.co", // Substitua pelo seu subdomínio real do Supabase
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzY3Jxd2VpZ3NidnludmZncndtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTA4OTMsImV4cCI6MjA5NjY4Njg5M30.9L-JiRGcVwoITtpdxdzyo9N2DIQvYHFa50UVdpsD31w"   // Substitua pela sua chave anon real do Supabase
+    SUPABASE_URL: "https://nfcmwsrfdshyfyvmwemh.supabase.co", // Substitua pelo seu subdomínio real do Supabase
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mY213c3JmZHNoeWZ5dm13ZW1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMzQ0OTgsImV4cCI6MjA5NjcxMDQ5OH0._TDLC93FKNKhV4FPUCB173HTMrr-OBciMojH_0Ccqx0"   // Substitua pela sua chave anon real do Supabase
 };
